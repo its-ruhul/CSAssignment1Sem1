b5d4519cac50af9a2cd0f94ee666f0c89b00901c
Ruhul <itsruhul@proton.me>
2025-09-20 17:32:07 +0530

    Added some missing questions by Ruhul

---
+++ ques66.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main(){
+
+    char item;
+    int i = 0;
+    int cost = 0;
+    int quantity = 0;
+
+
+    printf("Press any other key to exit. \n");
+    printf("(B = Burgers, S = Sandwiches, P = Pizza, F = French Fries)\n");
+
+    while( i < 4){
+
+        printf("Please give your orders: ");
+        scanf("%c", &item);
+
+        switch(item) {
+
+            case 'B':
+                printf("Enter the number of items: ");
+                scanf("%d", &quantity);
+
+                cost = cost + quantity * 200;
+                break;
+
+            case 'F':
+                printf("Enter the number of items: ");
+                scanf("%d", &quantity);
+
+                cost = cost + quantity * 50;
+                break;
+
+            case 'P':
+                printf("Enter the number of items: ");
+                scanf("%d", &quantity);
+
+                cost = cost + quantity * 500;
+                break;
+
+            case 'S':
+                printf("Enter the number of items: ");
+                scanf("%d", &quantity);
+
+                cost = cost + quantity * 15;
+                break;
+
+            default:
+                printf("default executed \n");
+                
+        }
+
+        i++;       
+    }
+
+    printf("The total cost is: %d \n", cost);
+}
