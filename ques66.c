885a07d71a7cbd260673ff2c6992b54329a62445
Ruhul <itsruhul@proton.me>
2025-09-20 20:13:28 +0530

    Some questions were enhanced

--- ques66.c
+++ ques66.c
@@ -11,7 +11,7 @@ int main(){
     printf("Press any other key to exit. \n");
     printf("(B = Burgers, S = Sandwiches, P = Pizza, F = French Fries)\n");
 
-    while( i < 4){
+    while( i == 0 ){
 
         printf("Please give your orders: ");
         scanf("%c", &item);
@@ -19,39 +19,48 @@ int main(){
         switch(item) {
 
             case 'B':
+            {
                 printf("Enter the number of items: ");
                 scanf("%d", &quantity);
 
                 cost = cost + quantity * 200;
                 break;
+            }
+
 
             case 'F':
+            {
                 printf("Enter the number of items: ");
                 scanf("%d", &quantity);
 
                 cost = cost + quantity * 50;
                 break;
+            }
 
             case 'P':
+            {
                 printf("Enter the number of items: ");
                 scanf("%d", &quantity);
 
                 cost = cost + quantity * 500;
                 break;
+            }
 
             case 'S':
+            {
                 printf("Enter the number of items: ");
                 scanf("%d", &quantity);
 
                 cost = cost + quantity * 15;
                 break;
+            }
 
             default:
+            {
                 printf("default executed \n");
-                
-        }
-
-        i++;       
+                i = 1;
+            }                
+        }      
     }
 
     printf("The total cost is: %d \n", cost);
