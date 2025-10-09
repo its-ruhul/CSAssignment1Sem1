4e038ca25dcaf4844e39cc3a047f6734e9e6ad51
Ruhul <itsruhul@proton.me>
2025-10-09 23:42:50 +0530

    developed logic for ques4 of assignment3

---
+++ assignment3/ques4.c
@@ -0,0 +1,20 @@
+//Convert Decimal to Binary
+
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+
+  int decimal;
+  int binary;
+  int digit;
+  int maxPower2;
+
+  printf("Enter the decimal no.: ");
+  scanf("%d", &decimal);
+
+  //max power of 2.
+  //to do that I shall make a loop which increments power of 2 till the
+  //power of 2 becomes more than the decimal.
+
+  printf("%d", maxPower2);
