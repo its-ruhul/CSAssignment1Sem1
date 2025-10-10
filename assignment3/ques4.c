814ce19c183125664215bcc98375ae403687a001
Ruhul <itsruhul@proton.me>
2025-10-10 14:22:04 +0530

    solved ques4.c

--- assignment3/ques4.c
+++ assignment3/ques4.c
@@ -1,14 +1,13 @@
 //Convert Decimal to Binary
 
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
 int main(){
 
   int decimal;
-  int binary;
-  int digit;
-  int maxPower2;
+  int binary = 0;
+  int remainder;
 
   printf("Enter the decimal no.: ");
   scanf("%d", &decimal);
@@ -17,5 +16,17 @@ int main(){
   //to do that I shall make a loop which increments power of 2 till the
   //power of 2 becomes more than the decimal.
 
-  printf("%d", maxPower2);
+  int i = 1;
+  while (decimal != 0){
+
+    remainder = decimal % 2;
+    decimal = decimal / 2;
+
+    binary = binary + (remainder * i);
+    i = i * 10;
+  }
+
+  printf("%d \n", binary);
+
+  return 0;
 }
