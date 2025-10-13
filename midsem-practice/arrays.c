e43ca5a5cd7562f879734a196d405b48131f68ba
Ruhul <itsruhul@proton.me>
2025-10-14 00:31:23 +0530

    solved questions

---
+++ midsem-practice/arrays.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+  int array[] = {0, 1, 2, 3};
+
+  array[4] = 4;
+  array[5] = 5;
+
+  printf("SIZE of array: %d \n", sizeof(array));
+
+  int i = 0;
+  while (i < 5){
+    printf("%d ", array[i]);
+    i++;
+  }
+
+  printf("\n");
+  return 0;
+}
