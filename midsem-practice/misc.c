385598899a070c49842ee2fdbdea40fb38eac084
Ruhul <itsruhul@proton.me>
2025-10-13 19:43:09 +0530

    practiced most of the things before arrays

---
+++ midsem-practice/misc.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+  FILE *fp;
+  
+  printf("Enter value using getchar(): ");
+  char name = getchar();      //getchar() requires you to enter a char and then enter.
+
+  printf("%d \n", name);
+
+  fp = fopen("forLoop.c", "r");
+
+  printf("Enter value using getc(): ");
+  char name2 = getc(fp);
+
+  //getch();
+  //its not a standard function in c now.
+  // use getc or getchar.
+
+  return 0;
+}
